cf82078474029e2b8efd4d1e5ef68d0f79364623
buwentian1 <1135480503@qq.com>
2025-12-24 00:05:58 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,15 +1,14 @@
 # 请在下方输入自定义命令(一般用来安装第三方插件)(可以留空)
 
 # 编辑默认的lan口ip地址
-sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
 # 编辑默认的主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # 编辑默认的luci显示的固件名称
-#sed -i 's/OpenWrt/ZWRT/g' package/base-files/files/bin/config_generate
-#sed -i 's/ImmortalWrt/ZWRT/g' package/base-files/files/bin/config_generate
-
+sed -i 's/OpenWrt/buwentian/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/buwentian/g' package/base-files/files/bin/config_generate
 # 添加额外的软件包，echo 方式和git clone 方式二选一即可
 #echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
@@ -20,5 +19,4 @@ git clone https://github.com/chenmozhijin/turboacc.git package/turboacc
 git clone https://github.com/stevenjoezhang/luci-app-adguardhome.git package/ADGH
 git clone https://github.com/buwentian1/istore.git package/istore
 git clone https://github.com/sirpdboy/luci-app-netspeedtest.git package/netspeedtest
-https://github.com/immortalwrt/Rclone-OpenWrt
 git clone https://github.com/immortalwrt/Rclone-OpenWrt.git package/Rclone
